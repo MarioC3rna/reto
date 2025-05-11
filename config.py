f59db2306ebf07f6bf0571dcfc329625da606ce6
Mario Cerna <159218404+MarioC3rna@users.noreply.github.com>
2025-05-11 07:51:08 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,1 @@
-#gitignore
 
-# Pega aquí tu API key (el código que copiaste)
-ROBOFLOW_API_KEY = "UduEjKEkicHx4mYTZIXz"
-
-# Este es el modelo que detecta personas
-ROBOFLOW_MODEL_ID = "coco/8"
-
-# La URL se construye automáticamente
-ROBOFLOW_API_URL = f"https://detect.roboflow.com/{ROBOFLOW_MODEL_ID}"
-
-# Configuración adicional
-CONFIDENCE_THRESHOLD = 0.4
-CAMERA_INDEX = 0
